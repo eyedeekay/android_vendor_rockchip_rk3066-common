a2c94513d410facefd58f2ddfb14988c3186c1cc
idk <problemsolver@openmailbox.org>
2018-01-30 01:14:35 -0500

    removed dumb shit like flash.

--- common-vendor-blobs.mk
+++ common-vendor-blobs.mk
@@ -78,16 +78,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/lib/hw/alsa.default.so:system/lib/hw/alsa.default.so
 
 #	$(LOCAL_PATH)/proprietary/lib/hw/audio.a2dp.default.so:system/lib/hw/audio.a2dp.default.so
-
-# Flash
-#PRODUCT_COPY_FILES += \
-#     $(LOCAL_PATH)/proprietary/app/FlashPlayer.apk:system/app/FlashPlayer.apk \
-#     $(LOCAL_PATH)/proprietary/lib/libflashplayer.so:system/lib/libflashplayer.so \
-#     $(LOCAL_PATH)/proprietary/lib/libysshared.so:system/lib/libysshared.so
-
-#PRODUCT_COPY_FILES += \
-#    $(LOCAL_PATH)/proprietary/app/RkVideoPlayer.apk:system/app/RkVideoPlayer.apk \
-#    $(LOCAL_PATH)/proprietary/app/RkExplorer.apk:system/app/RkExplorer.apk
-#    $(LOCAL_PATH)/proprietary/app/LauncherTV.apk:system/app/LauncherTV.apk
-#    $(LOCAL_PATH)/proprietary/app/RKBasicSettings.apk:system/app/RKBasicSettings.apk
-
